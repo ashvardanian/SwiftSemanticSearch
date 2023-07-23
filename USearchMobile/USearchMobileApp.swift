ef45f32a59137444a164ed44529d8ad5a8ee1110
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-07-23 20:32:51 +0400

    Initial Commit

---
+++ USearchMobile/USearchMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  USearchMobileApp.swift
+//  USearchMobile
+//
+//  Created by Ashot Vardanian on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct USearchMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
