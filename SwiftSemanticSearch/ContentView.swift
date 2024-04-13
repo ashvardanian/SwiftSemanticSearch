7e538b1385e40f9feb13f8020d4678059517437c
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-04-13 15:09:39 -0700

    Initial Commit

---
+++ SwiftSemanticSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSemanticSearch
+//
+//  Created by Ash Vardanian on 4/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSemanticSearch/SwiftSemanticSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSemanticSearchApp.swift
+//  SwiftSemanticSearch
+//
+//  Created by Ash Vardanian on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSemanticSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
