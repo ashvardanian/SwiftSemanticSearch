72782358b1540c9b32eb055f634f979fea3ade54
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-04-22 13:47:48 -0700

    Add: Grid of images

--- SwiftSemanticSearch/ContentView.swift
+++ SwiftSemanticSearch/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var imageModel: ImageModel
+    @State private var searchText = ""
+
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                TextField("Search", text: $searchText)
+                    .padding()
+                    .border(Color(UIColor.separator))
+
+                ScrollView {
+                    if imageModel.textModel == nil {
+                        ProgressView()
+                            .progressViewStyle(.circular)
+                    } else {
+                        LazyVGrid(columns: columns, spacing: 20) {
+                            ForEach(imageModel.filteredAndSortedImages(query: searchText), id: \.self) { imageName in
+                                if let imagePath = Bundle.main.path(forResource: imageName, ofType: nil, inDirectory: "images") {
+                                    Image(uiImage: UIImage(contentsOfFile: imagePath) ?? UIImage())
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 100)
+                                        .onTapGesture {
+                                            print("\(imageName) was tapped")
+                                        }
+                                }
+                            }
+                        }
+                        .padding()
+                    }
+                }
+            }
+            .navigationBarTitle("UForm + USearch 4 Swift", displayMode: .inline)
         }
-        .padding()
     }
 }
 
